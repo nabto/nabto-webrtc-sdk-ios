d79721d3f7407e9d36140848d8fd91f2b487401b
Ahmad Saleh <ahmadsaleh@quark.dev>
2025-05-14 12:48:14 +0200

    add fromJson to SignalingMessageUnion

--- Sources/NabtoWebRTCUtil/Schema.swift
+++ Sources/NabtoWebRTCUtil/Schema.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public enum SignalingMessageType: String, Codable {
     case description = "DESCRIPTION"
     case candidate = "CANDIDATE"
@@ -84,4 +86,31 @@ public struct SignalingMessageUnion {
     public var createRequest: SignalingCreateRequest? = nil
     public var createResponse: SignalingCreateResponse? = nil
     public var description: SignalingDescription? = nil
+
+    public static func fromJson(_ msg: String) -> SignalingMessageUnion {
+        let decoder = JSONDecoder()
+        let data = msg.data(using: .utf8)!
+
+        struct PossibleSignalingMessage: Codable {
+            var type: SignalingMessageType
+        }
+
+        let possibleMessage = try? decoder.decode(PossibleSignalingMessage.self, from: data)
+        var result = SignalingMessageUnion()
+        
+        switch possibleMessage?.type {
+            case .candidate:
+                result.candidate = try? decoder.decode(SignalingCandidate.self, from: data)
+            case .createRequest:
+                result.createRequest = try? decoder.decode(SignalingCreateRequest.self, from: data)
+            case .createResponse:
+                result.createResponse = try? decoder.decode(SignalingCreateResponse.self, from: data)
+            case .description:
+                result.description = try? decoder.decode(SignalingDescription.self, from: data)
+            case nil:
+                break
+        }
+
+        return result
+    }
 }
