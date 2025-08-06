56fd2dbf34bb6a445ad845e4d7e4137161c54f1a
Thomas <tfk@debian>
2025-08-06 11:41:13 +0200

    WebrtcSignalingMessage docs

--- Sources/NabtoWebRTCUtil/WebrtcSignalingMessage.swift
+++ Sources/NabtoWebRTCUtil/WebrtcSignalingMessage.swift
@@ -1,9 +1,22 @@
 import NabtoWebRTC
 
+/**
+ * Generalized WebRTC Signaling message to be sent/received by the
+ * MessageTransport. This message can contain either a SignalingDescription or a
+ * SignalingCandidate.
+ */
 public struct WebrtcSignalingMessage {
     public var candidate: SignalingCandidate?
     public var description: SignalingDescription?
 
+    /**
+     * Construct a WebRTC Signaling message from a SignalingCandidate or a SignalingDescription.
+     *
+     * A WebrtcSignalingMessage can only contain one of the two, so either the candidate or the description must be nil.
+     *
+     * @param candidate The candidate if this is a candidate.
+     * @param description The description if this is a description.
+     */
     public init(candidate: SignalingCandidate? = nil, description: SignalingDescription? = nil) {
         self.candidate = candidate
         self.description = description
