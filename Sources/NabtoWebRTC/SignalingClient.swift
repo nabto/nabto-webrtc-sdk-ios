fd7bba62c34168fac8f576db56cbbaca688622d1
Ulrik Gammelby <ug@nabto.com>
2025-11-18 16:10:27 +0100

    add missing documentation on state enum

--- Sources/NabtoWebRTC/SignalingClient.swift
+++ Sources/NabtoWebRTC/SignalingClient.swift
@@ -8,7 +8,7 @@
  *  - FAILED: The Signaling Connection has failed and will not reconnect.
  *  - CLOSED: The Signaling Connection was closed by the application.
  */
- public enum SignalingConnectionState: String {
+public enum SignalingConnectionState: String {
     case new = "NEW"
     case connecting = "CONNECTING"
     case connected = "CONNECTED"
@@ -21,6 +21,16 @@
  * Hint about the state of the remote peer. The state is only updated in
  * certain situations and in some cases it does not reflect the actual state
  * of the remote peer.
+ *
+ *  - NEW: The state is new if the client has not been connected yet.
+ *  - CONNECTED: The state is connected if our best guess is that the other peer
+ * is currently connected to the signaling service.
+ *  - DISCONNECTED: The state is disconnected if the current best guess is that
+ * the other peer is disconnected from the signaling service.
+ *  - FAILED: If the channel has received an error which is fail in the protocol
+ * the state is failed.
+ *  - CLOSED: If close has been called on the channel by the application or by
+ * the signaling client, the state is closed.
  */
 public enum SignalingChannelState : String {
     case new = "NEW"
