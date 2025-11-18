8a2c890dae52649477fd1d35c49d4b71da6b4b71
Ulrik Gammelby <ug@nabto.com>
2025-11-18 15:52:41 +0100

    fix documentation on connection state enum

--- Sources/NabtoWebRTC/SignalingClient.swift
+++ Sources/NabtoWebRTC/SignalingClient.swift
@@ -1,19 +1,14 @@
 /**
- * Hint about the state of the remote peer. The state is only updated in
- * certain situations and in some cases it does not reflect the actual state
- * of the remote peer.
+ * The different states the Signaling Connection can have.
  *
- *  - NEW: The state is new if the client has not been connected yet.
- *  - CONNECTED: The state is connected if our best guess is that the other peer
- * is currently connected to the signaling service.
- *  - DISCONNECTED: The state is disconnected if the current best guess is that
- * the other peer is disconnected from the signaling service.
- *  - FAILED: If the channel has received an error which is fail in the protocol
- * the state is failed.
- *  - CLOSED: If close has been called on the channel by the application or by
- * the signaling client, the state is closed.
+ *  - NEW: The Signaling Connection was just created
+ *  - CONNECTING: The Signaling Connection is connecting to the backend.
+ *  - CONNECTED: The Signaling Connection is connected and ready to use.
+ *  - WAIT_RETRY: The Signaling Connection is disconnected and waiting for its backoff before reconnecting.
+ *  - FAILED: The Signaling Connection has failed and will not reconnect.
+ *  - CLOSED: The Signaling Connection was closed by the application.
  */
-public enum SignalingConnectionState: String {
+ public enum SignalingConnectionState: String {
     case new = "NEW"
     case connecting = "CONNECTING"
     case connected = "CONNECTED"
